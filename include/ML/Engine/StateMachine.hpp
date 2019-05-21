69bedebdf836fb3a4cc4ba79ad46ddc5a24d64a2
Melody <gurman8r@gmail.com>
2019-05-20 20:16:15 -0400

    fix in StateMachine

--- include/ML/Engine/StateMachine.hpp
+++ include/ML/Engine/StateMachine.hpp
@@ -33,12 +33,11 @@ namespace ml
 		{
 			for (auto it = init.begin(); it != init.end(); it++)
 			{
-				if (it->second && (it->first != (static_cast<key_type>(ML_STATE_NONE))))
+				if (it->second && (it->first > (static_cast<key_type>(ML_STATE_NONE))))
 				{
 					m_states[it->first] = it->second;
 				}
 			}
-
 		}
 
 	public: // Get State
@@ -48,7 +47,7 @@ namespace ml
 		> inline fun_type operator[](const T & key)
 		{
 			const_iterator it;
-			return ((key != (static_cast<key_type>(ML_STATE_NONE)))
+			return ((key > (static_cast<key_type>(ML_STATE_NONE)))
 				? ((((it = m_states.find(static_cast<key_type>(key))) != m_states.end())
 					? (it->second)
 					: (NULL)))

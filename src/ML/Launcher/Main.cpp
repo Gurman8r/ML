732b47ef629e8261ca420f23013c814eac7f7e12
Melody <gurman8r@gmail.com>
2019-05-23 01:20:01 -0400

    main

--- src/ML/Launcher/Main.cpp
+++ src/ML/Launcher/Main.cpp
@@ -1,4 +1,4 @@
-/* * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <ML/Core/Debug.hpp>
 #include <ML/Core/EventSystem.hpp>
@@ -12,106 +12,102 @@
 #include <ML/Core/StateMachine.hpp>
 #include <ML/Graphics/RenderWindow.hpp>
 
-/* * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 # ifndef ML_CONFIG_INI
 # define ML_CONFIG_INI "../../../ML.ini"
 # endif
 
-/* * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 using namespace ml;
 
-/* * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
-int32_t main()
+static Preferences	g_Preferences	{ ML_CONFIG_INI };
+static EventSystem	g_EventSystem	{};
+static Resources	g_Resources		{};
+static GameTime		g_Time			{};
+static RenderWindow g_Window		{ g_EventSystem };
+static Engine		g_Engine		{ g_EventSystem };
+static Editor		g_Editor		{ g_EventSystem };
+
+enum State { Enter, Loop, Exit };
+
+static StateMachine<State> g_ControlFlow =
 {
-	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-	static Preferences	g_Preferences	{ ML_CONFIG_INI };
-	static EventSystem	g_EventSystem	{};
-	static Resources	g_Resources		{};
-	static GameTime		g_Time			{};
-	static RenderWindow g_Window		{ g_EventSystem };
-	static Engine		g_Engine		{ g_EventSystem };
-	static Editor		g_Editor		{ g_EventSystem };
-
-	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
-
-	enum State { Enter, Loop, Exit };
-
-	static StateMachine<State> g_ControlFlow =
+{ State::Enter, []()
+{	/* Enter */
+	g_EventSystem.fireEvent(EnterEvent(
+		g_Preferences,
+		g_Window
+	));
+	/* Load Content */
+	g_EventSystem.fireEvent(LoadEvent(
+		g_Preferences,
+		g_Resources
+	));
+	/* Start */
+	g_EventSystem.fireEvent(StartEvent(
+		g_Time,
+		g_Resources,
+		g_Window
+	));
+	return g_ControlFlow(State::Loop);
+} },
+{ State::Loop, []()
+{	/* Loop */
+	while (g_Window.isOpen())
 	{
-	{ State::Enter, []()
-	{	/* Enter */
-		g_EventSystem.fireEvent(EnterEvent(
-			g_Preferences,
-			g_Window
-		));
-		/* Load Content */
-		g_EventSystem.fireEvent(LoadEvent(
-			g_Preferences,
-			g_Resources
-		));
-		/* Start */
-		g_EventSystem.fireEvent(StartEvent(
+		/* Begin Frame */
+		g_Time.beginLoop();
+		g_Window.pollEvents();
+
+		/* Update */
+		g_EventSystem.fireEvent(UpdateEvent(
 			g_Time,
 			g_Resources,
 			g_Window
 		));
-		return g_ControlFlow(State::Loop);
-	} },
-	{ State::Loop, []()
-	{	/* Loop */
-		while (g_Window.isOpen())
-		{
-			/* Begin Frame */
-			g_Time.beginLoop();
-			g_Window.pollEvents();
 
-			/* Update */
-			g_EventSystem.fireEvent(UpdateEvent(
-				g_Time,
-				g_Resources,
-				g_Window
-			));
-
-			/* Draw */
-			g_EventSystem.fireEvent(DrawEvent(
-				g_Time,
-				g_Resources,
-				g_Window
-			));
-
-			/* Gui */
-			g_EventSystem.fireEvent(BeginGuiEvent());
-			g_EventSystem.fireEvent(GuiEvent(
-				g_Time,
-				g_Resources,
-				g_Editor
-			));
-			g_EventSystem.fireEvent(EndGuiEvent());
-
-			/* End Frame */
-			g_Window.swapBuffers();
-			g_Time.endLoop();
-		}
-		return g_ControlFlow(State::Exit);
-	} },
-	{ State::Exit, []()
-	{	/* Exit */
-		g_EventSystem.fireEvent(ExitEvent(
+		/* Draw */
+		g_EventSystem.fireEvent(DrawEvent(
+			g_Time,
 			g_Resources,
 			g_Window
 		));
-		return g_ControlFlow.NoState;
-	} },
-	};
 
-	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+		/* Gui */
+		g_EventSystem.fireEvent(BeginGuiEvent());
+		g_EventSystem.fireEvent(GuiEvent(
+			g_Time,
+			g_Resources,
+			g_Editor
+		));
+		g_EventSystem.fireEvent(EndGuiEvent());
 
+		/* End Frame */
+		g_Window.swapBuffers();
+		g_Time.endLoop();
+	}
+	return g_ControlFlow(State::Exit);
+} },
+{ State::Exit, []()
+{	/* Exit */
+	g_EventSystem.fireEvent(ExitEvent(
+		g_Resources,
+		g_Window
+	));
+	return g_ControlFlow.NoState;
+} },
+};
+
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+int32_t main()
+{
 	// Load Plugins
-	static Map<SharedLibrary *, Plugin *> g_Plugins;
+	Map<SharedLibrary *, Plugin *> plugins;
 	if (auto file = std::ifstream(ML_FS.getPathTo(g_Preferences.GetString(
 		"Launcher",
 		"plugin_list",
@@ -129,7 +125,7 @@ int32_t main()
 				.replaceAll("$(PlatformTarget)", ML_PLATFORM_TARGET)
 			));
 
-			auto plugin = g_Plugins.insert({
+			auto plugin = plugins.insert({
 				library,
 				library->callFunction<Plugin *>(ML_str(ML_Plugin_Main), g_EventSystem)
 			}).first->second;
@@ -141,7 +137,7 @@ int32_t main()
 	g_ControlFlow(State::Enter);
 
 	// Cleanup Plugins
-	for (auto & pair : g_Plugins)
+	for (auto & pair : plugins)
 	{
 		if (pair.second) { delete pair.second; }
 		if (pair.first)  { delete pair.first;  }
@@ -149,8 +145,6 @@ int32_t main()
 
 	// Goodbye!
 	return EXIT_SUCCESS;
-
-	/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 }
 
-/* * * * * * * * * * * * * * * * * * * * */
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */

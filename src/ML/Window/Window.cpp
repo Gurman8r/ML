3a76db3c7eb29d176759eafe2d09519305665777
Melody <gurman8r@gmail.com>
2019-06-19 20:13:57 -0400

    formatting

--- src/ML/Window/Window.cpp
+++ src/ML/Window/Window.cpp
@@ -41,19 +41,19 @@ namespace ml
 			}
 		}
 #endif
-		eventSystem.addListener(CharEvent::ID, this);
-		eventSystem.addListener(CursorEnterEvent::ID, this);
-		eventSystem.addListener(CursorPosEvent::ID, this);
-		eventSystem.addListener(FrameSizeEvent::ID, this);
-		eventSystem.addListener(KeyEvent::ID, this);
-		eventSystem.addListener(MouseButtonEvent::ID, this);
-		eventSystem.addListener(ScrollEvent::ID, this);
-		eventSystem.addListener(WindowCloseEvent::ID, this);
-		eventSystem.addListener(WindowErrorEvent::ID, this);
-		eventSystem.addListener(WindowFocusEvent::ID, this);
-		eventSystem.addListener(WindowKillEvent::ID, this);
-		eventSystem.addListener(WindowSizeEvent::ID, this);
-		eventSystem.addListener(WindowPosEvent::ID, this);
+		eventSystem.addListener(CharEvent::ID,			this);
+		eventSystem.addListener(CursorEnterEvent::ID,	this);
+		eventSystem.addListener(CursorPosEvent::ID,		this);
+		eventSystem.addListener(FrameSizeEvent::ID,		this);
+		eventSystem.addListener(KeyEvent::ID,			this);
+		eventSystem.addListener(MouseButtonEvent::ID,	this);
+		eventSystem.addListener(ScrollEvent::ID,		this);
+		eventSystem.addListener(WindowCloseEvent::ID,	this);
+		eventSystem.addListener(WindowErrorEvent::ID,	this);
+		eventSystem.addListener(WindowFocusEvent::ID,	this);
+		eventSystem.addListener(WindowKillEvent::ID,	this);
+		eventSystem.addListener(WindowSizeEvent::ID,	this);
+		eventSystem.addListener(WindowPosEvent::ID,		this);
 	}
 	
 	Window::~Window() 

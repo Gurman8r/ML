141214e64c973697f027c257bed4a82d58507896
Melody <gurman8r@gmail.com>
2019-08-16 17:22:25 -0400

    update boot

--- assets/scripts/boot.py
+++ assets/scripts/boot.py
@@ -4,8 +4,7 @@ import memelib as ml
 
 if (__name__ == "__main__"):
     if ml.config.is_debug:
-        ml.io.printl("MemeLib " + ml.config.project_ver)
-        ml.io.printl(ml.config.project_url)
+        ml.io.printl("MemeLib " + ml.config.project_ver + " (" + ml.config.project_url + ")")
 
     ml.plugins.load("Noobs_$(Configuration)_$(PlatformTarget).dll")
     ml.plugins.load("CommandSuite_$(Configuration)_$(PlatformTarget).dll")

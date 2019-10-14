1aab3d9bef1221d05bea126d4641f0d23f45169f
Melody <gurman8r@gmail.com>
2019-10-14 18:21:30 -0400

    moved tools

--- premake5.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-call .\premake5\Windows\premake5.exe vs2019
-if %ERRORLEVEL% NEQ 0 ( pause )
---
+++ tools/premake5.bat
@@ -0,0 +1,4 @@
+@echo off
+cd ..\
+call .\tools\premake5\Windows\premake5.exe vs2019
+if %ERRORLEVEL% NEQ 0 ( pause )

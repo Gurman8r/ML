a45cf458783c49a6592d2ce45f3fbe17b484413d
Melody <gurman8r@gmail.com>
2019-10-15 17:22:37 -0400

    update tools

--- tools/Build_Debug_x64.bat
+++ tools/Build_Debug_x64.bat
@@ -1,9 +1,24 @@
 @echo off
-rem Build Debug x64
+rem Build Release x64
 cls
-call Build_64.bat ^
- "%cd%\..\ML.sln" ^
+set CWD=%cd%\
+pushd %CWD%
+
+rem set version=2017\Enterprise
+set version=2019\Community
+
+set env_path=%ProgramFiles(x86)%\Microsoft Visual Studio\%version%\
+set aux_path=%env_path%VC\Auxiliary\Build\
+
+cd %aux_path%
+call vcvars64.bat
+
+cd %env_path%
+call msbuild.exe "%CWD%..\ML.sln" ^
  "/p:Configuration=Debug" ^
  "/p:PlatformTarget=x64" ^
  "/p:PlatformToolset=v142" ^
- "/p:Subsystem=Console"
+ "/p:Subsystem=Windows"
+
+popd
+if %ERRORLEVEL% NEQ 0 ( pause )

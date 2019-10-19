9f4b3cabba36f98330b39eb35ea0f5ecf6e08461
Melody <gurman8r@gmail.com>
2019-10-19 18:56:28 -0400

    update build

---
+++ build/Build.bat
@@ -0,0 +1,46 @@
+@echo off
+cls
+set CWD=%cd%\
+pushd %CWD%
+
+rem 2015 / 2017 / 2019
+set vs_version=%1
+
+rem Enterprise / Community
+set vs_release=%2
+
+rem Debug / Release
+set config=%3
+
+rem x86 / x64
+set platform=%4
+
+rem v140 / v141 / v142
+set toolset=%5
+
+rem Console / Windows
+set subsystem=%6
+
+echo Version 	Visual Studio %vs_version% %vs_release%
+echo Configure 	%config%
+echo Platform 	%platform%
+echo Toolset 	%toolset%
+echo Subsystem 	%subsystem%
+
+set msbuild_path=%ProgramFiles(x86)%\Microsoft Visual Studio\%vs_version%\%vs_release%\
+
+cd %msbuild_path%VC\Auxiliary\Build\
+if "%platform%" == "x86" 	( call "vcvarsall.bat" x86 )
+if "%platform%" == "x64" 	( call "vcvarsall.bat" x64 )
+if "%platform%" == "Win32" 	( call "vcvarsall.bat" x86 )
+if "%platform%" == "Win64" 	( call "vcvarsall.bat" x64 )
+
+cd %msbuild_path%
+call msbuild.exe "%CWD%..\ML.sln" ^
+ "/p:Configuration=%config%" ^
+ "/p:PlatformTarget=%platform%" ^
+ "/p:PlatformToolset=%toolset%" ^
+ "/p:Subsystem=%subsystem%"
+
+popd
+if %ERRORLEVEL% NEQ 0 ( pause )
--- build/Build_Release_x64.bat
+++ build/Build_Release_x64.bat
@@ -1,24 +1,3 @@
 @echo off
-rem Build Release x64
-cls
-set CWD=%cd%\
-pushd %CWD%
-
-rem set version=2017\Enterprise
-set version=2019\Community
-
-set env_path=%ProgramFiles(x86)%\Microsoft Visual Studio\%version%\
-set aux_path=%env_path%VC\Auxiliary\Build\
-
-cd %aux_path%
-call vcvars64.bat
-
-cd %env_path%
-call msbuild.exe "%CWD%..\ML.sln" ^
- "/p:Configuration=Release" ^
- "/p:PlatformTarget=x64" ^
- "/p:PlatformToolset=v142" ^
- "/p:Subsystem=Windows"
-
-popd
-if %ERRORLEVEL% NEQ 0 ( pause )
+call .\Build.bat 2019 Community Release x64 v142 Windows
+exit

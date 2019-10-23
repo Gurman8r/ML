19633e197c7f93eb74dcbea8b545fad9a071e52d
Melody <gurman8r@gmail.com>
2019-10-22 23:10:04 -0400

    move profiler dock

--- src/ML/Editor/Editor.cpp
+++ src/ML/Editor/Editor.cpp
@@ -58,7 +58,7 @@ namespace ml
 			{
 				EditorDockspace & d { ev->dockspace };
 				d.dockWindow(m_explorer	.getTitle(), d.getNode(d.RightUp));
-				d.dockWindow(m_profiler	.getTitle(), d.getNode(d.RightUp));
+				d.dockWindow(m_profiler	.getTitle(), d.getNode(d.LeftDn));
 				d.dockWindow(m_inspector.getTitle(), d.getNode(d.RightUp));
 				d.dockWindow(m_content	.getTitle(), d.getNode(d.RightDn));
 				d.dockWindow(m_terminal	.getTitle(), d.getNode(d.LeftDn));

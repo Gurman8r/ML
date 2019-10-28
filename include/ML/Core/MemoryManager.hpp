1ff90dd57c5da124eeb0e226a00c53611f82b6b7
Melody <gurman8r@gmail.com>
2019-10-28 01:34:17 -0400

    nvm

--- include/ML/Core/MemoryManager.hpp
+++ include/ML/Core/MemoryManager.hpp
@@ -27,7 +27,7 @@ namespace ml
 			friend ML_SERIALIZE(std::ostream & out, const Record & value);
 		};
 
-		using RecordMap = typename Map<void *, Record *>;
+		using RecordMap = typename HashMap<void *, Record *>;
 
 		/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 

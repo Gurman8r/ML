f5da1a2216ea976e4f37d2aae2cdec8b0ff6664b
Melody <gurman8r@gmail.com>
2019-11-12 05:25:46 -0500

    build

--- examples/Noobs/Noobs.lua
+++ examples/Noobs/Noobs.lua
@@ -1,6 +1,6 @@
 -- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * --
 
-group "Examples"
+group "Plugins"
 project "Noobs"
 	targetname 		"%{prj.name}"
 	targetdir		"%{bin_lib}"

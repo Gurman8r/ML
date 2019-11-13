25577915899ad5108c1abec608990fd11835dadd
Melody <gurman8r@gmail.com>
2019-11-13 15:02:18 -0500

    update

--- tools/Update.bat
+++ tools/Update.bat
@@ -1,45 +1,27 @@
 @echo off
+cd ..\ext
 git submodule sync
 
-if not exist "..\ext\imgui" ( git submodule add --force -b docking https://github.com/Gurman8r/imgui.git )
-
-if not exist "..\ext\assimp" ( git submodule add --force https://github.com/Gurman8r/assimp.git )
-
-if not exist "..\ext\inih" ( git submodule add --force https://github.com/Gurman8r/inih.git )
-
-if not exist "..\ext\ImGuiColorTextEdit" ( git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git )
-
-if not exist "..\ext\cpython" ( git submodule add --force https://github.com/python/cpython.git )
-
-if not exist "..\ext\stb" ( git submodule add --force https://github.com/nothings/stb.git )
-
-if not exist "..\ext\pybind11" ( git submodule add --force https://github.com/pybind/pybind11.git )
-
-if not exist "..\ext\glm" ( git submodule add --force https://github.com/g-truc/glm.git )
-
-if not exist "..\ext\gcem" ( git submodule add --force https://github.com/kthohr/gcem.git )
-
-if not exist "..\ext\openal-soft" ( git submodule add --force https://github.com/kcat/openal-soft.git )
-
-if not exist "..\ext\dirent" ( git submodule add --force https://github.com/tronkko/dirent.git )
-	
-if not exist "..\ext\freetype2" ( git submodule add --force https://github.com/aseprite/freetype2.git )
-
-if not exist "..\ext\PDCurses" ( git submodule add --force https://github.com/wmcbrine/PDCurses.git )
-
-if not exist "..\ext\mega-sdk" ( git submodule add --force https://github.com/meganz/sdk.git mega-sdk )
-
-if not exist "..\ext\ImGuizmo" ( git submodule add --force https://github.com/CedricGuillemet/ImGuizmo.git )
-
-if not exist "..\ext\premake-core" ( git submodule add --force https://github.com/premake/premake-core.git )
-
-if not exist "..\ext\glew" ( git submodule add --force https://github.com/nigels-com/glew.git )
-
-if not exist "..\ext\glfw" ( git submodule add --force https://github.com/Gurman8r/glfw.git )
-
-if not exist "..\ext\lua" ( git submodule add --force https://github.com/lua/lua.git )
-
-if not exist "..\ext\RakNet" ( git submodule add --force https://github.com/facebookarchive/RakNet.git )
+git submodule add --force -b docking https://github.com/Gurman8r/imgui.git
+git submodule add --force https://github.com/Gurman8r/assimp.git
+git submodule add --force https://github.com/Gurman8r/inih.git
+git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
+git submodule add --force https://github.com/python/cpython.git
+git submodule add --force https://github.com/nothings/stb.git
+git submodule add --force https://github.com/pybind/pybind11.git
+git submodule add --force https://github.com/g-truc/glm.git
+git submodule add --force https://github.com/kthohr/gcem.git
+git submodule add --force https://github.com/kcat/openal-soft.git
+git submodule add --force https://github.com/tronkko/dirent.git
+git submodule add --force https://github.com/aseprite/freetype2.git
+git submodule add --force https://github.com/wmcbrine/PDCurses.git
+git submodule add --force https://github.com/meganz/sdk.git mega-sdk
+git submodule add --force https://github.com/CedricGuillemet/ImGuizmo.git
+git submodule add --force https://github.com/premake/premake-core.git
+git submodule add --force https://github.com/nigels-com/glew.git
+git submodule add --force https://github.com/Gurman8r/glfw.git
+git submodule add --force https://github.com/lua/lua.git
+git submodule add --force https://github.com/facebookarchive/RakNet.git
 
 git submodule update --init --recursive
 git pull
--- tools/Update.sh
+++ tools/Update.sh
@@ -1,45 +1,27 @@
 #!/bin/bash
+cd ../ext
 git submodule sync
 
-[ ! -d "../ext/imgui" ] && git submodule add --force -b docking https://github.com/Gurman8r/imgui.git
-
-[ ! -d "../ext/assimp" ] && git submodule add --force https://github.com/Gurman8r/assimp.git
-
-[ ! -d "../ext/inih" ] && git submodule add --force https://github.com/Gurman8r/inih.git
-
-[ ! -d "../ext/ImGuiColorTextEdit" ] && git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
-
-[ ! -d "../ext/cpython" ] && git submodule add --force https://github.com/python/cpython.git
-
-[ ! -d "../ext/stb" ] && git submodule add --force https://github.com/nothings/stb.git
-
-[ ! -d "../ext/pybind11" ] && git submodule add --force https://github.com/pybind/pybind11.git
-
-[ ! -d "../ext/glm" ] && git submodule add --force https://github.com/g-truc/glm.git
-
-[ ! -d "../ext/gcem" ] && git submodule add --force https://github.com/kthohr/gcem.git
-
-[ ! -d "../ext/openal-soft" ] && git submodule add --force https://github.com/kcat/openal-soft.git
-
-[ ! -d "../ext/dirent" ] && git submodule add --force https://github.com/tronkko/dirent.git
-	
-[ ! -d "../ext/freetype2" ] && git submodule add --force https://github.com/aseprite/freetype2.git
-
-[ ! -d "../ext/PDCurses" ] && git submodule add --force https://github.com/wmcbrine/PDCurses.git
-
-[ ! -d "../ext/mega-sdk" ] && git submodule add --force https://github.com/meganz/sdk.git mega-sdk
-
-[ ! -d "../ext/ImGuizmo" ] && git submodule add --force https://github.com/CedricGuillemet/ImGuizmo.git
-
-[ ! -d "../ext/premake-core" ] && git submodule add --force https://github.com/premake/premake-core.git
-
-[ ! -d "../ext/glew" ] && git submodule add --force https://github.com/nigels-com/glew.git
-
-[ ! -d "../ext/glew" ] && git submodule add --force https://github.com/Gurman8r/glfw.git
-
-[ ! -d "../ext/lua" ] && git submodule add --force https://github.com/lua/lua.git
-
-[ ! -d "../ext/RakNet" ] && git submodule add --force https://github.com/facebookarchive/RakNet.git
+git submodule add --force -b docking https://github.com/Gurman8r/imgui.git
+git submodule add --force https://github.com/Gurman8r/assimp.git
+git submodule add --force https://github.com/Gurman8r/inih.git
+git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
+git submodule add --force https://github.com/python/cpython.git
+git submodule add --force https://github.com/nothings/stb.git
+git submodule add --force https://github.com/pybind/pybind11.git
+git submodule add --force https://github.com/g-truc/glm.git
+git submodule add --force https://github.com/kthohr/gcem.git
+git submodule add --force https://github.com/kcat/openal-soft.git
+git submodule add --force https://github.com/tronkko/dirent.git
+git submodule add --force https://github.com/aseprite/freetype2.git
+git submodule add --force https://github.com/wmcbrine/PDCurses.git
+git submodule add --force https://github.com/meganz/sdk.git mega-sdk
+git submodule add --force https://github.com/CedricGuillemet/ImGuizmo.git
+git submodule add --force https://github.com/premake/premake-core.git
+git submodule add --force https://github.com/nigels-com/glew.git
+git submodule add --force https://github.com/Gurman8r/glfw.git
+git submodule add --force https://github.com/lua/lua.git
+git submodule add --force https://github.com/facebookarchive/RakNet.git
 
 git submodule update --init --recursive
 git pull

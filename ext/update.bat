4ebbfd0dc246938a3811af58548c415c0237f91f
Melody <gurman8r@gmail.com>
2019-11-13 01:52:41 -0500

    test

--- ext/update.bat
+++ ext/update.bat
@@ -1,21 +1,41 @@
 @echo off
 
-rem git submodule add --force -b docking https://github.com/ocornut/imgui.git
-rem git submodule add --force https://github.com/Gurman8r/assimp.git
-rem git submodule add --force https://github.com/Gurman8r/inih.git
-rem git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
-rem git submodule add --force https://github.com/python/cpython.git
-rem git submodule add --force https://github.com/nothings/stb.git
-rem git submodule add --force https://github.com/pybind/pybind11.git
-rem git submodule add --force https://github.com/g-truc/glm.git
-rem git submodule add --force https://github.com/kthohr/gcem.git
-rem git submodule add --force https://github.com/kcat/openal-soft.git
-rem git submodule add --force https://github.com/tronkko/dirent.git
-rem git submodule add --force https://github.com/aseprite/freetype2.git
-rem git submodule add --force https://github.com/wmcbrine/PDCurses
-rem git submodule add --force https://github.com/meganz/sdk.git
-rem git submodule add --force https://github.com/CedricGuillemet/ImGuizmo
-rem git submodule add --force https://github.com/premake/premake-core.git
-rem git submodule add --force https://github.com/nigels-com/glew
-rem git submodule add --force https://github.com/lua/lua
-rem git submodule add --force https://github.com/facebookarchive/RakNet
+if not exist ".\imgui" ( git submodule add -b docking https://github.com/ocornut/imgui.git )
+
+if not exist ".\assimp" ( git submodule add https://github.com/Gurman8r/assimp.git )
+
+if not exist ".\inih" ( git submodule add https://github.com/Gurman8r/inih.git )
+
+if not exist ".\ImGuiColorTextEdit" ( git submodule add https://github.com/Gurman8r/ImGuiColorTextEdit.git )
+
+if not exist ".\cpython" ( git submodule add https://github.com/python/cpython.git )
+
+if not exist ".\stb" ( git submodule add https://github.com/nothings/stb.git )
+
+if not exist ".\pybind11" ( git submodule add https://github.com/pybind/pybind11.git )
+
+if not exist ".\glm" ( git submodule add https://github.com/g-truc/glm.git )
+
+if not exist ".\gcem" ( git submodule add https://github.com/kthohr/gcem.git )
+
+if not exist ".\openal-soft" ( git submodule add https://github.com/kcat/openal-soft.git )
+
+if not exist ".\dirent" ( git submodule add https://github.com/tronkko/dirent.git )
+	
+if not exist ".\freetype2" ( git submodule add https://github.com/aseprite/freetype2.git )
+
+if not exist ".\PDCurses" ( git submodule add https://github.com/wmcbrine/PDCurses.git )
+
+if not exist ".\mega-sdk" ( git submodule add https://github.com/meganz/sdk.git mega-sdk )
+
+if not exist ".\ImGuizmo" ( git submodule add https://github.com/CedricGuillemet/ImGuizmo.git )
+
+if not exist ".\premake-core" ( git submodule add https://github.com/premake/premake-core.git )
+
+if not exist ".\glew" ( git submodule add https://github.com/nigels-com/glew.git )
+
+if not exist ".\lua" ( git submodule add https://github.com/lua/lua.git )
+
+if not exist ".\RakNet" ( git submodule add https://github.com/facebookarchive/RakNet.git )
+
+if %ERRORLEVEL% NEQ 0" ( pause )
--- ext/update.sh
+++ ext/update.sh
@@ -1,21 +1,39 @@
 #!/bin/bash
 
-# git submodule add --force -b docking https://github.com/ocornut/imgui.git
-# git submodule add --force https://github.com/Gurman8r/assimp.git
-# git submodule add --force https://github.com/Gurman8r/inih.git
-# git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
-# git submodule add --force https://github.com/python/cpython.git
-# git submodule add --force https://github.com/nothings/stb.git
-# git submodule add --force https://github.com/pybind/pybind11.git
-# git submodule add --force https://github.com/g-truc/glm.git
-# git submodule add --force https://github.com/kthohr/gcem.git.git
-# git submodule add --force https://github.com/kcat/openal-soft.git
-# git submodule add --force https://github.com/tronkko/dirent.git
-# git submodule add --force https://github.com/aseprite/freetype2.git
-# git submodule add --force https://github.com/wmcbrine/PDCurses.git
-# git submodule add --force https://github.com/meganz/sdk.git
-# git submodule add --force https://github.com/CedricGuillemet/ImGuizmo.git
-# git submodule add --force https://github.com/premake-core.git
-# git submodule add --force https://github.com/nigels-com/glew.git
-# git submodule add --force https://github.com/lua/lua.git
-# git submodule add --force https://github.com/facebookarchive/RakNet.git
+[ ! -d "./imgui" ] && git submodule add -b docking https://github.com/ocornut/imgui.git
+
+[ ! -d "./assimp" ] && git submodule add https://github.com/Gurman8r/assimp.git
+
+[ ! -d "./inih" ] && git submodule add https://github.com/Gurman8r/inih.git
+
+[ ! -d "./ImGuiColorTextEdit" ] && git submodule add https://github.com/Gurman8r/ImGuiColorTextEdit.git
+
+[ ! -d "./cpython" ] && git submodule add https://github.com/python/cpython.git
+
+[ ! -d "./stb" ] && git submodule add https://github.com/nothings/stb.git
+
+[ ! -d "./pybind11" ] && git submodule add https://github.com/pybind/pybind11.git
+
+[ ! -d "./glm" ] && git submodule add https://github.com/g-truc/glm.git
+
+[ ! -d "./gcem" ] && git submodule add https://github.com/kthohr/gcem.git
+
+[ ! -d "./openal-soft" ] && git submodule add https://github.com/kcat/openal-soft.git
+
+[ ! -d "./dirent" ] && git submodule add https://github.com/tronkko/dirent.git
+	
+[ ! -d "./freetype2" ] && git submodule add https://github.com/aseprite/freetype2.git
+
+[ ! -d "./PDCurses" ] && git submodule add https://github.com/wmcbrine/PDCurses.git
+
+[ ! -d "./mega-sdk" ] && git submodule add https://github.com/meganz/sdk.git mega-sdk
+
+[ ! -d "./ImGuizmo" ] && git submodule add https://github.com/CedricGuillemet/ImGuizmo.git
+
+[ ! -d "./premake-core" ] && git submodule add https://github.com/premake/premake-core.git
+
+[ ! -d "./glew" ] && git submodule add https://github.com/nigels-com/glew.git
+
+[ ! -d "./lua" ] && git submodule add https://github.com/lua/lua.git
+
+[ ! -d "./RakNet" ] && git submodule add https://github.com/facebookarchive/RakNet.git

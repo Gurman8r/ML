85220a345c127079928b779ae6a285898dfd44c8
Melody <gurman8r@gmail.com>
2019-11-13 00:48:34 -0500

    test

--- ext/install.bat
+++ ext/install.bat
@@ -1,19 +1,20 @@
-git submodule add -b docking https://github.com/ocornut/glfw.git
-git submodule add https://github.com/Gurman8r/assimp.git
-git submodule add https://github.com/Gurman8r/inih.git
-git submodule add https://github.com/Gurman8r/ImGuiColorTextEdit.git
-git submodule add https://github.com/python/cpython.git
-git submodule add https://github.com/nothings/stb.git
-git submodule add https://github.com/pybind/pybind11.git
-git submodule add https://github.com/g-truc/glm.git
-git submodule add https://github.com/kthohr/gcem.git
-git submodule add https://github.com/kcat/openal-soft.git
-git submodule add https://github.com/tronkko/dirent.git
-git submodule add hhttps://github.com/aseprite/freetype2.git
-git submodule add hhttps://github.com/wmcbrine/PDCurses
-git submodule add hhttps://github.com/meganz/sdk.git
-git submodule add hhttps://github.com/CedricGuillemet/ImGuizmo
-git submodule add hhttps://github.com/premake/premake-core.git
-git submodule add hhttps://github.com/nigels-com/glew
-git submodule add hhttps://github.com/lua/lua
-git submodule add hhttps://github.com/facebookarchive/RakNet
+@echo off
+git submodule add --force -b docking https://github.com/ocornut/imgui.git
+rem git submodule add --force https://github.com/Gurman8r/assimp.git
+rem git submodule add --force https://github.com/Gurman8r/inih.git
+rem git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
+rem git submodule add --force https://github.com/python/cpython.git
+git submodule add --force https://github.com/nothings/stb.git
+git submodule add --force https://github.com/pybind/pybind11.git
+rem git submodule add --force https://github.com/g-truc/glm.git
+rem git submodule add --force https://github.com/kthohr/gcem.git
+rem git submodule add --force https://github.com/kcat/openal-soft.git
+rem git submodule add --force https://github.com/tronkko/dirent.git
+rem git submodule add --force hhttps://github.com/aseprite/freetype2.git
+rem git submodule add --force hhttps://github.com/wmcbrine/PDCurses
+git submodule add --force hhttps://github.com/meganz/sdk.git
+rem git submodule add --force hhttps://github.com/CedricGuillemet/ImGuizmo
+git submodule add --force hhttps://github.com/premake/premake-core.git
+rem git submodule add --force hhttps://github.com/nigels-com/glew
+git submodule add --force hhttps://github.com/lua/lua
+rem git submodule add --force hhttps://github.com/facebookarchive/RakNet

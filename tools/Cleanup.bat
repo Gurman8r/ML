b3592ea4e0debda7b45466560fb8a7554d7e0301
Melody <gurman8r@gmail.com>
2019-11-13 03:04:33 -0500

    tools

---
+++ tools/Cleanup.bat
@@ -0,0 +1,5 @@
+@echo off
+cd ..\
+for /d /r ..\ %%d in ("__pycache__") do @if exist "%%d" rd /s /q "%%d"
+for /d /r ..\ %%d in ("imgui.ini") do @if exist "%%d" del /q "%%d"
+exit
---
+++ tools/Uninstall.bat
@@ -0,0 +1,17 @@
+@echo off
+
+if exist ..\.vs rd /s /q ..\.vs
+
+if exist ..\bin rd /s /q ..\bin
+
+if exist ..\bin-lib rd /s /q ..\bin-lib
+
+if exist ..\bin-obj rd /s /q ..\bin-obj
+
+if exist ..\proj rd /s /q ..\proj
+
+if exist ..\ML_vs2019.sln del /q ..\ML_vs2019.sln
+
+for /d /r ..\ %%d in ("__pycache__") do @if exist "%%d" rd /s /q "%%d"
+
+if %ERRORLEVEL% NEQ 0 ( pause )

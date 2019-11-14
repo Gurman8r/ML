7cb70789d139340f4167af8f05a28d0272d50540
Melody <gurman8r@gmail.com>
2019-11-14 12:40:15 -0500

    deps

--- tools/Update_Git.bat
+++ tools/Update_Git.bat
@@ -6,7 +6,7 @@ git submodule add --force -b docking https://github.com/Gurman8r/imgui.git
 git submodule add --force https://github.com/Gurman8r/assimp.git
 git submodule add --force https://github.com/Gurman8r/inih.git
 git submodule add --force https://github.com/Gurman8r/ImGuiColorTextEdit.git
-git submodule add --force https://github.com/Gurman8r/glfw.git
+git submodule add --force https://github.com/glfw/glfw.git
 git submodule add --force https://github.com/python/cpython.git
 git submodule add --force https://github.com/nothings/stb.git
 git submodule add --force https://github.com/pybind/pybind11.git
